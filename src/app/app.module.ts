4c158be2266dbafa652166b85b15aa5f1573dce0
lucasmss <lucasmss16@gmail.com>
2022-07-21 20:54:20 -0300

    Ajuste layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,11 @@ import { ContatoComponent } from './contato/contato.component';
 import { ContatoService } from './shared/service/contato.service';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
-import { HttpClientModule } from '@angular/common/http'
-import { ReactiveFormsModule } from '@angular/forms'
-
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -25,6 +27,9 @@ import { ReactiveFormsModule } from '@angular/forms'
     MatInputModule,
     HttpClientModule,
     ReactiveFormsModule,
+    MatIconModule,
+    MatTableModule,
+    MatTabsModule,
     BrowserAnimationsModule
   ],
   providers: [
